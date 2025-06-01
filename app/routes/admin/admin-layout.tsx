fc37a48175b8bb87f9890ff3628e28e2d11fcea4
oniafk <manufelipearias@gmail.com>
2025-06-01 17:50:37 +1000

    navbar, NavItems, StatsCard, TripCard components

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router";
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import { NavItems } from "../../../components";
+import MobileSidebar from "components/MobileSidebar";
 
 const AdminLayout = () => {
   return (
     <div className="admin-layout">
-      MobileSidebar
+      <MobileSidebar />
       <aside className="w-full max-w-[270px] hidden lg:block">
-        {/* <SidebarComponent width={270} enableGestures={false}>
-                    <NavItems />
-                </SidebarComponent> */}
+        <SidebarComponent width={270} enableGestures={false}>
+          <NavItems />
+        </SidebarComponent>
       </aside>
       <aside className="children">
         <Outlet />
